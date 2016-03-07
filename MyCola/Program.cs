ce887a9e6d3105e06c344bb16e8113924ef4040f
Mahendra <mahendra@proitsolutions.lk>
2016-03-07 15:04:01 +0530

    Update with the structure

--- MyCola/Program.cs
+++ MyCola/Program.cs
@@ -15,7 +15,7 @@ namespace MyCola
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new login());
         }
     }
 }
